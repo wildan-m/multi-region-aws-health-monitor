2fa73b844d4db7ed2f32aa7fa80b75da67878577
Wildan Muhlis <muhliswildan@gmail.com>
2025-07-30 21:25:06 +0700

    init

---
+++ terraform/variables.tf
@@ -0,0 +1,63 @@
+variable "project_name" {
+  description = "Name of the project"
+  type        = string
+  default     = "sleek-health-monitor"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "db_instance_class" {
+  description = "RDS instance class"
+  type        = string
+  default     = "db.t3.micro"
+}
+
+variable "monitoring_enabled" {
+  description = "Enable detailed monitoring"
+  type        = bool
+  default     = true
+}
+
+variable "tags" {
+  description = "Common tags for all resources"
+  type        = map(string)
+  default = {
+    Project     = "sleek-health-monitor"
+    ManagedBy   = "terraform"
+  }
+}
